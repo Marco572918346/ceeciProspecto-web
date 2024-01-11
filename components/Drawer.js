5aed300e936157520dd7ffa3b515a24603fb0ae1
MichelValian <michel14.valian@gmail.com>
2024-01-11 10:53:51 -0600

    Correccion de ruta del Drawer en app.js

---
+++ components/Drawer.js
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from "react";
+// import MiniDrawerEmployee from "./MiniDrawerEmployee";
+import MiniDrawer from "./MiniDrawer";
+
+const Drawer = (props) => {
+    return (
+      <MiniDrawer>
+        {props.children}
+      </MiniDrawer>
+    );
+};
+ export default Drawer;
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
 import '@/styles/globals.css'
+import Drawer from '../../components/Drawer';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Drawer>
+      <Component {...pageProps} />
+    </Drawer>
+  );
+  
 }
+
