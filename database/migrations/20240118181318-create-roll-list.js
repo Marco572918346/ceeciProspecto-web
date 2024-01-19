427ded43ec6cba7e6e1969a5dd230b449eb96b2f
EduardoRosas05 <rosasvega56@gmail.com>
2024-01-19 11:59:17 -0600

    modificaciones de backend

---
+++ database/migrations/20240118181318-create-roll-list.js
@@ -0,0 +1,40 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('RollLists', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      attendance: {
+        type: Sequelize.BOOLEAN, 
+        //allowNull:false,
+      },
+      studentId: {
+        type: Sequelize.INTEGER,
+        allowNull:false,
+        references: {
+          model: 'Users',
+          key: 'id'
+        }
+      },
+      date: {
+        type: Sequelize.DATEONLY
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('RollLists');
+  }
+};
