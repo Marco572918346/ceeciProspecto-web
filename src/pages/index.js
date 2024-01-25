c361f80053760c50f9980bc6465b9a9fccb141a9
Marco572918346 <marcoantonio.07102@gmail.com>
2024-01-25 10:18:21 -0600

    Restauracion de la imagen

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ function Home() {
       <CardMedia
         component="img"
         alt="Imagen"
-        image="https://scontent-dfw5-2.xx.fbcdn.net/v/t39.30808-6/255926978_3258503637807867_1804716959916837386_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=3635dc&_nc_ohc=_B-eFyS30YcAX_DxCP4&_nc_ht=scontent-dfw5-2.xx&oh=00_AfDbDJPrZhXaUGc7nW_P2RtjS9sYaAkJ9nUW47Dvc14bHw&oe=65A58750"
+        image='ceeciPropaganda.jpg'
         sx={{
           width: 1000,
           margin: "auto",
